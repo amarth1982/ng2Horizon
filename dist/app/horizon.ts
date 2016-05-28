fa3f6e4cc662d5221430e2d2a97f214ad0e40443
Anandha Marthandan <amarth1982@live.com>
2016-05-28 00:14:06 -0400

    added more type definitions

--- dist/app/horizon.ts
+++ dist/app/horizon.ts
@@ -1,4 +1,26 @@
 
+import {Observable} from 'rxjs/observable'
+
+declare interface IStatus{
+  type: string
+}
+
+export declare interface  IHorizon {
+  connect():void
+
+  disconnect():void
+
+  onReady():Observable<IStatus>
+
+  status():Observable<IStatus>
+
+  onDisconnected(): Observable<IStatus>
+
+  onSocketError(): Observable<IStatus>
+}
+
+declare function Horizon(): IHorizon;
+
 export const HORIZON = 'HORIZON'
 
 export let horizon = Horizon()
